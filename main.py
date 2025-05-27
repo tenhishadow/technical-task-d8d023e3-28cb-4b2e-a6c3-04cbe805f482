129c03d932bb110d7ec200aa3a75e1ae06cbae48
tenhishadow <adm@tenhi.dev>
2025-05-27 16:25:51 +0200

    main.py: configure unsued vars

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ env = os.getenv("ENV")
 if env is None:
     raise RuntimeError("Missing required ENV environment variable!")
 
-log_level = os.getenv("LOG_LEVEL", "debug")
+# allow reload only for local env
+RELOAD = env == "local"
+
+log_level = os.getenv("LOG_LEVEL", "info")
 database_url = os.getenv("DATABASE_URL", "sqlite://memory")
 timeout_seconds = int(os.getenv("TIMEOUT_SECONDS", "30"))
 
 @app.on_event("startup")
 async def startup_event():
-    time.sleep(15)
+    time.sleep(timeout_seconds)
     app.state.healthy = True
 
 @app.get("/health")
@@ -38,4 +41,4 @@ async def create_data(item: dict):
     return {"received": item}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=8085, reload=RELOAD, log_level=log_level)
